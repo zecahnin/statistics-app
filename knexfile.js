13161f5f9246df0739c722ad4a628240ceee246b
Sandy Mycrosan <sandysantos@mycrosan.com.br>
2017-11-20 19:23:48 -0200

    Ajustes na conexao

--- knexfile.js
+++ knexfile.js
@@ -1,17 +1,27 @@
-// Update with your config settings.
-
-module.exports = {
-  production: {
-    client: 'mysql',
-    connection: {
-      database: 'production_api',
-      user:     'root',
-      password: 'zeca',
-      host: 'localhost',
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+// module.exports = {
+//   production: {
+//     client: 'mysql',
+//     connection: {
+//       database: 'production_api',
+//       user: 'root',
+//       password: 'password',
+//       host: 'localhost',
+//     },
+//     migrations: {
+//       tableName: 'knex_migrations'
+//     }
+//   }
+// };
+var knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    database: 'production_api',
+    user: 'root',
+    password: 'password',
+    host: 'localhost',
+  },
+  migrations: {
+    tableName: 'knex_migrations'
   }
-
-};
+});
+module.exports = knex
